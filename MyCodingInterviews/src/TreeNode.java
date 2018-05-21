fc4ff4760cfe2d6cfb3be3e5cac4e81a0a4df71b
ParetoFront <2461365998@qq.com>
2018-05-21 22:43:00 +0800

    补充二叉树的print和insert方法

--- MyCodingInterviews/src/TreeNode.java
+++ MyCodingInterviews/src/TreeNode.java
@@ -6,4 +6,29 @@ public class TreeNode {
 	TreeNode(int x){
 		this.val=x;
 	}
+	public static void insert(TreeNode t,int x) {
+		if(x>t.val) {
+			if(t.right==null) {
+				t.right=new TreeNode(x);
+			}else {
+				insert(t.right,x);
+			}
+			
+		}else if(x<t.val) {
+			if(t.left==null) {
+				t.left=new TreeNode(x);
+			}else {
+				insert(t.left,x);
+			}
+		}else {
+			
+		}
+	}
+	 public static void printTree(TreeNode node) {
+	        if (node != null) {
+	            printTree(node.left);
+	            System.out.print(node.val + " ");
+	            printTree(node.right);
+	        }
+	    }
 }
